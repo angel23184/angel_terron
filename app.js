90782e3800cfacf037e0ec9b8fd28803a45078ce
aterron84 <aterron84@gmail.com>
2019-01-10 14:33:11 +0100

    Connected to Mongo with a set timeout

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-const express = require('express');
+require('dotenv').config();
+const express = require("express");
 const app = express();
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+setTimeout(() => {
+  mongoose.Promise = Promise;
+  mongoose
+    .connect("mongodb://angel_terron_mongodb_1/cabifybootcamp")
+    .then(x => {
+      console.log(`Connected to Mongo!`);
+    })
+    .catch(err => {
+      console.error("Error connecting to mongo", err);
+    });
+}, 15000);
 
-mongoose.Promise = Promise;
-mongoose
-  .connect(process.env.DBURL)
-  .then(x => {
-    console.log(`Connected to Mongo!`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function(req, res) {
+  res.send("Hello World!");
 });
 
-
-app.listen(9001, function () {
-  console.log('Example app listening on port 9001!');
+app.listen(9001, function() {
+  console.log("Example app listening on port 9001!");
 });
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/messages', require('./messageRoute'));
+app.use("/messages", require("./messageRoute"));
 
 module.exports = app;
