d6488cc4c22b5af5d18fe24319abfa276cd5d11e
aterron84 <aterron84@gmail.com>
2019-01-15 12:59:54 +0100

    Iteracion 1

--- app.js
+++ app.js
@@ -5,16 +5,30 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dbName = "cabifybootcamp";
 //const server = "angel_terron_mongodb_1"
-const server = "localhost"
+const serverL1 = "localhost:27017"
+const serverL2 = "localhost:27018"
 
 
 const trm =setInterval(() => {
   mongoose.Promise = Promise;
   mongoose
-    .connect(`mongodb://${server}/${dbName}`)
+    .createConnection(`mongodb://${serverL1}/${dbName}`)
     .then(x => {
       console.log(`Connected to Mongo!`);
       clearInterval(trm)
+    })
+    .catch(err => {
+      console.error("Error connecting to mongo", err);
+    });
+}, 1000);
+
+const trm2 =setInterval(() => {
+  mongoose.Promise = Promise;
+  mongoose
+    .createConnection(`mongodb://${serverL2}/${dbName}`)
+    .then(x => {
+      console.log(`Connected to Mongo!`);
+      clearInterval(trm2)
     })
     .catch(err => {
       console.error("Error connecting to mongo", err);
