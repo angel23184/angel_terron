0774f4f9436587f751953e822f91090f98669f28
aterron84 <aterron84@gmail.com>
2019-01-22 16:32:23 +0100

    Separate BBDD for each service. Iteracion 1, two bullets completed

---
+++ serviceCredit/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 9017;
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, function() {
+  console.log("Example app listening on port 9001!");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/credit", require("../routes/creditRoutes"));
+
+
+module.exports = app;
--- serviceMessage/app.js
+++ serviceMessage/app.js
@@ -2,21 +2,20 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const PORT = 9001;
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
 });
 
-app.listen(9001, function() {
+app.listen(PORT, function() {
   console.log("Example app listening on port 9001!");
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/messages", require("./routes/messageRoute"));
-app.use("/credit", require("./routes/creditRoutes"));
-app.use("/messages", require("./routes/messageStatus"));
+app.use("/messages", require("../routes/messageRoute"));
+app.use("/messages", require("../routes/messageStatus"));
 
 module.exports = app;
